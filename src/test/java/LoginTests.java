a95ab901fe6165d2e343f5627f2c380591e788a1
VovanOzz <120048518+Vovanozz@users.noreply.github.com>
2023-03-07 22:31:50 +0200

    HW_9

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,55 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase {
+    @Test
+    public void loginPositiveTest(){
+        String email="v762900819@gmail.com";
+        String password="Vova1234$";
+        openLoginRegistrationForm();
+        fillLoginRegistrationForm(email,password);
+        submitLogin();
+        pause(5000);
+        Assert.assertTrue(isLogged());
+        Assert.assertTrue(isElementPresent(By.xpath("//a[@href='/contacts']")));
+    }
+    @Test
+    public void loginNegativeTestEmail(){
+        String email="v762900819gmail.com";
+        String password="Vova1234$";
+        openLoginRegistrationForm();
+        fillLoginRegistrationForm(email,password);
+        submitLogin();
+        pause(5000);
+        Assert.assertFalse(isLogged());
+        Assert.assertFalse(isElementPresent(By.xpath("//a[@href='/contacts']")));
+    }
+    @Test
+    public void loginNegativeTestPassword(){
+        String email="v762900819@gmail.com";
+        String password="Vov";
+        openLoginRegistrationForm();
+        fillLoginRegistrationForm(email,password);
+        submitLogin();
+        pause(5000);
+        Assert.assertFalse(isLogged());
+        Assert.assertFalse(isElementPresent(By.xpath("//a[@href='/add']")));
+    }
+    @Test
+    public void loginUnregisterUserTest(){
+        String email="vov19@gmail.com";
+        String password="Vov1234$";
+        openLoginRegistrationForm();
+        fillLoginRegistrationForm(email,password);
+        submitLogin();
+        pause(5000);
+        Assert.assertFalse(isLogged());
+        Assert.assertFalse(isElementPresent(By.xpath("//a[@href='/add']")));
+
+    }
+
+
+
+
+}
--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -2,81 +2,86 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import static org.testng.Assert.assertEquals;
 
-public class RegistrationTests {
+public class RegistrationTests extends TestBase{
     WebDriver wd;
-
-    @BeforeMethod
-    public void init(){
-        wd=new ChromeDriver();
-        wd.navigate().to("https://telranedu.web.app/home");
-    }
+   @BeforeMethod
+   public void preCondition(){
+       if(isLogged()){
+           logout();
+       }
+   }
 
     @Test
     public void regPositiveTest(){
-        wd.findElement(By.xpath("//a[@href='/login']")).click();
 
+        openLoginRegistrationForm();
         int i= (int) ((System.currentTimeMillis()/1000)%3600);
+       String email="vov"+i+"@gmail.com";
+       String password="Vov12345$";
+        fillLoginRegistrationForm(email,password);
+        submitRegistration();
+        pause(5000);
+        Assert.assertTrue(isLogged());
+        Assert.assertTrue(isElementPresent(By.xpath("//button[.='Sign Out']")));
 
-        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-       emailInput.click();
-      emailInput.clear();
-       emailInput.sendKeys("vov"+i+"@gmail.com");
+    }
 
-        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-        passInput.click();
-        passInput.clear();
-        passInput.sendKeys("Vov12345$");
-        wd.findElement(By.xpath("//button[2]")).click();
-        //Assert.assertTrue(wd.findElement(By.xpath("//a[text='ADD']")).getText().equals("ADD"));
+    @Test
+    public void WrongEmailRegistration(){
+       openLoginRegistrationForm();
+        int i= (int) ((System.currentTimeMillis()/1000)%3600);
+        String email="vov"+i+"gmail.com";
+        String password="Vov12345$";
+        fillLoginRegistrationForm(email,password);
+        submitRegistration();
+        pause(5000);
+        Assert.assertFalse(isLogged());
+        Assert.assertTrue(isElementPresent(By.xpath("//div[.='Registration failed with code 400']")));
+
+
 
     }
     @Test
-    public void WrongEmailRegistrationTest(){
-       wd.findElement(By.xpath("//a[@href='/login']")).click();
-         WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-         emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("vovgmailcom");
-
-        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-        passInput.click();
-        passInput.clear();
-        passInput.sendKeys("Vov12345$");
-        wd.findElement(By.xpath("//button[2]")).click();
-
+    public void WrongPasswordRegistration(){
+        openLoginRegistrationForm();
+        int i= (int) ((System.currentTimeMillis()/1000)%3600);
+        String email="vov"+i+"gmail.com";
+        String password="vov";
+        fillLoginRegistrationForm(email,password);
+        submitRegistration();
+        pause(5000);
+        Assert.assertFalse(isLogged());
+        Assert.assertTrue(isElementPresent(By.xpath("//div[.='Registration failed with code 400']")));
     }
     @Test
-    public void WrongPasswordRegistrationTest(){
-        wd.findElement(By.xpath("//a[@href='/login']")).click();
+    public void registrationUserAlreadyExists(){
+        openLoginRegistrationForm();
 
-        int i= (int) ((System.currentTimeMillis()/1000)%3600);
-
-        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("vov"+i+"@gmail.com");
-
-        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-        passInput.click();
-        passInput.clear();
-        passInput.sendKeys("vov");
-        wd.findElement(By.xpath("//button[2]")).click();
+        String email="v762900819@gmail.com";
+        String password="Vova1234$";
+        fillLoginRegistrationForm(email,password);
+        submitRegistration();
+        pause(5000);
+        Assert.assertFalse(isLogged());
+        Assert.assertTrue(isElementPresent(By.xpath("//div[.='Registration failed with code 409']")));
     }
 
 
-
-
-    @AfterMethod
-    public void tearDown(){
-        //wd.quit();
-    }
+//    @AfterMethod
+//    public void tearDown(){
+//        //wd.quit();
+//    }
 
 
 
